7b19d8526df0d489659c7f4c71ad9d3fc2d37ffd
David Chen <bdfd2005@gmail.com>
2019-12-03 23:23:19 -0500

    initial set up

--- express/server.js
+++ express/server.js
@@ -2,8 +2,22 @@
 const express = require('express');
 const path = require('path');
 const serverless = require('serverless-http');
-const app = express();
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const ejs= require('ejs')
+const expressLayouts = require('express-ejs-layouts');
+const cors = require('cors');
+const indexRouter = require('./routes/index');
+const functionsRouter = require('./routes/functions');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.set('layout', 'layouts/layout');
 
 const router = express.Router();
 router.get('/', (req, res) => {
@@ -14,9 +28,21 @@ router.get('/', (req, res) => {
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
+
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use('/', indexRouter);
+app.use('/functions', functionsRouter);
+
 
 module.exports = app;
 module.exports.handler = serverless(app);
